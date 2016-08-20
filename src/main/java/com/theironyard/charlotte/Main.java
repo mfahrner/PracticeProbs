3d6af8f806374dc87d4833c149a1c95853c49e83
michael fahrner <Mike Fahrner>
2016-08-19 22:40:48 -0400

    21 down

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -160,7 +160,7 @@ public class Main {
 //    and big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks.
 //    This is a little harder than it looks and can be done without any loops.
 
-    public static boolean makeBricks(int small, int big, int goal) {
+    public boolean makeBricks(int small, int big, int goal) {
         int bigBrick = 5;
         int firstStep = goal % (big * bigBrick);
 
@@ -171,6 +171,149 @@ public class Main {
             return false;
         }
     }
+
+//    Given 2 int values greater than 0, return whichever value is nearest to 21 without going over.
+//    Return 0 if they both go over.
+
+    public int blackjack(int a, int b) {
+        int highNumber = Integer.max(a, b);
+        if (a > 21 && b > 21) {
+            System.out.println("Both Bust!");
+        }
+        else if (highNumber > 21) {
+            return Integer.min(a, b);
+        }
+            return highNumber;
+    }
+
+//    Given 3 int values, a b c, return their sum.
+//    However, if one of the values is the same as another of the values, it does not count towards the sum.
+
+    public int loneSum(int a, int b, int c) {
+
+        if (a == b && b == c) {
+            return 0;
+        }
+        else if (a == b) {
+            return c;
+        }
+        else if (b == c) {
+            return a;
+        }
+        else if (a == c) {
+            return b;
+        }
+        else {
+            return a + b + c;
+        }
+    }
+
+//    For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
+//    so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is
+//    less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values.
+//    To avoid code repetition, write a separate helper "public int round10(int num) {" and call it 3 times.
+//    Write the helper entirely below and at the same indent level as roundSum().
+
+    public int round10(int num) {
+        int remainder = num % 10;
+        if (remainder >= 5) {
+            int roundUp = num + (10 - remainder);
+            return roundUp;
+        }
+        else {
+            int roundDown = num - remainder;
+            return roundDown;
+        }
+    }
+
+    public int roundSum(int a, int b, int c) {
+        int answer = (round10(a) + round10(b) + round10(c));
+        return answer;
+    }
+
+//    Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19
+//    inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper
+//    "public int fixTeen(int n) {"that takes in an int value and returns that value fixed for the teen rule.
+//    In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+//    Define the helper below and at the same indent level as the main noTeenSum().
+
+    public int fixTeen(int n) {
+        if ((n == 13 || n == 14) || (n>= 17 && n <= 19)) {
+            n = 0;
+            return Integer.valueOf(n);
+        }
+        else {
+            return Integer.valueOf(n);
+        }
+    }
+
+    public int noTeenSum(int a, int b, int c) {
+        int answer = (fixTeen(a) + fixTeen(b) + fixTeen(c));
+        return answer;
+    }
+//
+//    Given three ints, a b c, one of them is small, one is medium and one is large. Return true if the three values are
+//    evenly spaced, so the difference between small and medium is the same as the difference between medium and large
+
+    public boolean evenlySpaced(int a, int b, int c) {
+        int[] spaced = {a, b, c};
+        Arrays.sort(spaced, 0, 3);
+        int firstDiff = spaced[1] - spaced [0];
+        int secondDiff = spaced[2] - spaced [1];
+        if (firstDiff == secondDiff) {
+            return true;
+        }
+            return false;
+    }
+
+//    Given 3 int values, a b c, return their sum. However, if one of the values is 13
+//    then it does not count towards the sum and values to its right do not count. So for example,
+//    if b is 13, then both b and c do not count.
+
+    public int luckySum(int a, int b, int c) {
+        if (a == 13) {
+            return 0;
+        }
+        else if (b == 13) {
+            return a;
+        }
+        else if (c == 13) {
+            return a + b;
+        }
+        else {
+            return a + b + c;
+        }
+    }
+
+//    Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other
+//    is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
+
+    public static boolean closeFar(int a, int b, int c) {
+        int first = Math.abs(a - b);
+        int second = Math.abs(a - c);
+        if ((first == 1 || second == 1) && (first > 2 || second > 2)) {
+            return true;
+        }
+        return false;
+    }
+
+//    We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars (5 kilos each).
+//    Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+//    I thought this was worded terribly.  I'm assuming that if you use no small bars that = it can't be done?
+
+    public static int makeChocolate(int small, int big, int goal) {
+        int remainder = goal % 5;
+        if (remainder == 0) {
+            return -1;
+        }
+        return remainder;
+    }
+
+
+
+
+
+
 
 
 
